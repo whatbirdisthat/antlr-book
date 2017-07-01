edb4ce35a0ba427bb041d7b50273973c00040905
What Bird Is That? <whatbirdisthat@gmail.com>
2017-07-02 07:41:25 +1000

    add everthing new

--- projects/hello-world/antlr/java/run-test.sh
+++ projects/hello-world/antlr/java/run-test.sh
@@ -1,1 +1,15 @@
-echo 'hello Lucas!' | grun Hello r -tokens
+echo '
+Hello Grammar!
+______________
+
+Testing the "Hello" grammar.
+
+This grammar is set up to fail on text
+that is upper case, or is punctuation.
+
+It will only recognise lower case letters.
+
+Testing the phrase "hello Lucas!" which should fail on "L" and "!"
+
+'
+echo 'hello Lucas!' | java org.antlr.v4.gui.TestRig Hello r -tokens
